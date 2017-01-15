62fbeb77569cbe55c9db2bc35049c1f4db791ce4
Luan Nico <luanpotter27@gmail.com>
2017-01-15 20:13:01 -0200

    Adding test for complete cache (wip)

--- test/main.js
+++ test/main.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 const memoize = require('../src/main');
 
-let fib = (i) => {
-  if (i < 2) {
-    return 0;
-  }
-  return fib(i - 1) + fib(i - 2);
-};
-
 describe('#memoize', function() {
 
   it('should keep behaviour', function() {
+    let fib = (i) => {
+      if (i < 2) {
+        return 1;
+      }
+      return fib(i - 1) + fib(i - 2);
+    };
+
     assert.equal(fib(7), memoize(fib)(7));
   });
 
@@ -29,6 +29,30 @@ describe('#memoize', function() {
     assert.equal(4, fn(2, 2));
     assert.equal(4, fn(2, 2));
     assert.equal(1, hiddenSideEffect);
+  });
+
+  it('should update recursively', function() {
+    let hiddenSideEffect = 0;
+    let fib = (i) => {
+      hiddenSideEffect++;
+      if (i < 2) {
+        return 1;
+      }
+      return fib(i - 1) + fib(i - 2);
+    };
+    let fn = memoize(fib);
+
+    assert.equal(3, fn(3));
+    assert.equal(5, hiddenSideEffect);
+
+    hiddenSideEffect = 0;
+    assert.equal(3, fn(3));
+    assert.equal(0, hiddenSideEffect);
+
+    // TODO : this
+    // hiddenSideEffect = 0;
+    // assert.equal(2, fn(2));
+    // assert.equal(0, hiddenSideEffect);
   });
 
 });
